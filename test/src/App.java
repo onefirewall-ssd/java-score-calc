89c213e2b2633e93df9564ce47830854521b2132
OneFirewall User <join@onefirewall.com>
2021-08-05 21:32:10 +0100

    update

--- test/src/App.java
+++ test/src/App.java
@@ -2,17 +2,19 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 public class App {
     public static void main(String[] args) throws Exception {
-
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        System.out.println(timestamp.getTime()/1000);                      // 2021-03-24 16:34:26.666
-
-        System.out.println("A:" + calc_live_score(100) );
+        long ts = 1628193107; // This is the TS value of each entry
+        double score = 100.00032; // This is the SCORE value of each entry
+        double live_score = calc_live_score(score, ts); // This is the live score
     }
 
-    public static double calc_live_score(double score){
-        // return (scoreTimeZero) / (1 + Math.exp( (3/(scoreTimeZero)) * ((current_time/3600) - (2.5 * scoreTimeZero))))
-        return 0.32;
+    public static double calc_live_score(double scoreTimeZero, long ts){
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        double current_time = timestamp.getTime()/1000 - ts;
+        double live_score = (scoreTimeZero) / (1 + Math.exp( (3/(scoreTimeZero)) * ((current_time/3600) - (2.5 * scoreTimeZero))));
+        return live_score;
+
     }
 }
